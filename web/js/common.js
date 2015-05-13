5bd96bc305fcb381ab992b07df040e4a4552a2dd
root <root@raspberrypi.(none)>
2015-05-12 23:21:50 -0300

    chart auto refresh

--- web/js/common.js
+++ web/js/common.js
@@ -1,4 +1,4 @@
-
+// ----------- temperatures  ----------- 
 function settempvalue(value){
     value+="";
     if ( value.indexOf('.') == 0 ) {
@@ -23,7 +23,68 @@ function getresult() {
   }  
 }
 
+
+
+
+
+// ----------- charts  ----------- 
+window.setInterval(function(){
+    drawCurveTypes()
+}, 1000 * 60 * 5 );
+
+google.load('visualization', '1', {packages: ['corechart', 'line']});
+google.setOnLoadCallback(drawCurveTypes);
+
+function loadJSON(url) {
+    var request = new XMLHttpRequest();
+
+  // load it
+  // the last "false" parameter ensures that our code will wait before the
+  // data is loaded
+  request.open('GET', url, false);
+  request.send();
+
+  // parse adn return the output
+  return JSON.parse(request.responseText);
+};
+
+
+function drawCurveTypes() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Hour');
+  data.addColumn('number', 'Temp1');
+//      data.addColumn('number', 'Temp2');
+
+  data.addRows(loadJSON('/day/'));
+
+  var options = {
+    backgroundColor: { fill:'transparent' },
+    curveType: 'function',
+    hAxis: {
+      title: 'Hour'
+    },
+    vAxis: {
+      title: 'Temperature'
+    },
+    series: {
+      1: {curveType: 'function'}
+    }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('chartdiv'));
+  chart.draw(data, options);
+}
+
+// ----------- auto refresh  ----------- 
+ChartCounterDelay=0;
 setInterval(function() {
     val = getresult();
     settempvalue (  val );
-}, 1 * 1000); // 60 * 1000 milsec
+    ChartCounterDelay++;
+
+    if ( ChartCounterDelay == 100  ) {
+       ChartCounterDelay=0;
+       drawCurveTypes();
+    }
+
+}, 3 * 1000); // 60 * 1000 milsec
