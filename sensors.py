723904960a3271a6c345034a593408c23b859060
root <root@raspberrypi.(none)>
2015-06-29 18:28:47 -0300

    changed DHT lib to Adafruit_DHT

--- sensors.py
+++ sensors.py
@@ -1,7 +1,7 @@
 import os
 
 import time
-import dhtreader
+import Adafruit_DHT
 
 from time import gmtime, strftime
 from w1thermsensor import W1ThermSensor
@@ -9,24 +9,17 @@ from models import Sensors
 
 
 dhtpin=17
-dev_type=22
-
-dhtreader.init()
-
-
-time.sleep(10)
-
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 sensor = W1ThermSensor()	
 while True:
-    try:
-        sensor2_value_t, sensor2_value_h = dhtreader.read(dev_type, dhtpin)
-        sensor1_value = sensor.get_temperature()
-        datetime=strftime("%d-%m-%Y %H:%M", time.localtime())
-        Sensors().InsertData(sensor1_value,sensor2_value_t, sensor2_value_h, datetime)
-        time.sleep(60)
-    except ValueError:
-       print "Oops!  That was no valid number.  Try again..."
+    sensor2_value_t, sensor2_value_h = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
+    if sensor2_value_t  and sensor2_value_h:
+       sensor1_value = sensor.get_temperature()
+       datetime=strftime("%d-%m-%Y %H:%M", time.localtime())
+       Sensors().InsertData(sensor1_value,sensor2_value_t, sensor2_value_h, datetime)
+       time.sleep(60)
+    else:
+       time.sleep(5)
