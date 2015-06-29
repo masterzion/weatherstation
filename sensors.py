0bf5e72c7614c577d48eae17223a1bb4abe9996e
root <root@raspberrypi.(none)>
2015-06-29 18:39:02 -0300

    dht temperature and humidity in wrong order

--- sensors.py
+++ sensors.py
@@ -15,7 +15,7 @@ os.system('modprobe w1-therm')
  
 sensor = W1ThermSensor()	
 while True:
-    sensor2_value_t, sensor2_value_h = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
+    sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
     if sensor2_value_t  and sensor2_value_h:
        sensor1_value = sensor.get_temperature()
        datetime=strftime("%d-%m-%Y %H:%M", time.localtime())
