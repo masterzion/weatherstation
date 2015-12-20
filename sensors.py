f5d22f0d8be6f0af63439c412f652eebc81d0498
root <root@raspberrypi.(none)>
2015-12-20 15:38:55 -0200

    added log, check it temperature is lower than 50 (wrong read)

--- sensors.py
+++ sensors.py
@@ -13,13 +13,22 @@ dhtpin=17
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
-sensor = W1ThermSensor()	
+sensor = W1ThermSensor()
+sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
 while True:
     sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
-    if sensor2_value_t  and sensor2_value_h:
+    if ( sensor2_value_t ) > 60:
+       time.sleep(5)
+       sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
+    print sensor2_value_h, sensor2_value_t
+
+    if sensor2_value_t  and sensor2_value_h:      
        sensor1_value = sensor.get_temperature()
        datetime=strftime("%d-%m-%Y %H:%M", time.localtime())
-       Sensors().InsertData(sensor1_value,sensor2_value_t, sensor2_value_h, datetime)
+       print sensor1_value
+       if sensor1_value < 60 :
+          Sensors().InsertData(sensor1_value,sensor2_value_t, sensor2_value_h, datetime)
+          print sensor1_value,sensor2_value_t, sensor2_value_h, datetime
        time.sleep(60)
     else:
        time.sleep(5)
