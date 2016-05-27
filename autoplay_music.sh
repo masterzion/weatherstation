321c876eeb64e7741768bfb0f7f906a328fb420f
root <root@raspberrypi.(none)>
2016-05-27 20:37:14 +0200

    auto music player

---
+++ autoplay_music.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# 
+# require:  mpc
+# apt-get install mpc
+
+
+
+# saver to add in .bashrc ;)
+# MOBILE_IP=""
+# SERVER_NAME="password@ip" 
+SERVER_PORT="6600"
+
+echo "Mobile: "
+echo $MOBILE_IP
+echo "Server:"
+echo $SERVER_NAME
+
+function get_state() {
+    COUNT=$(ping "$MOBILE_IP" -c 3 | grep "ttl=" | wc -l)
+
+    if [ "$COUNT" == "0" ]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+
+LASTSTATE="-1"
+#while true
+#do
+while true;
+do
+    # get the actual state
+    STATE=$(get_state)
+
+    if [ "$LASTSTATE" == "$STATE" ]; then
+        # if found state wasn't changed, wait 3 seconds.
+        sleep 3
+    else
+        # if found state was changed
+        if [ "$STATE" == "0" ]; then
+           # it the state result was not found, pause the music
+           SEND_COMMAND="pause"
+        else
+           # else means the state result was found, play the music
+           SEND_COMMAND="toggle" #play
+        fi
+
+        #send command to the server
+        mpc -h "$SERVER_NAME" -p "$SERVER_PORT" "$SEND_COMMAND"
+
+        # save last state
+        LASTSTATE=$STATE
+    fi
+done
