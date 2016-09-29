6fa7981d3559b9b9d2d196d145e8df845fbb8cc3
root <master.zion@gmail.com>
2016-09-29 02:26:00 +0200

    added test_DS18B20.py

--- sensors.py
+++ sensors.py
@@ -8,7 +8,7 @@ from w1thermsensor import W1ThermSensor
 from models import Sensors
 
 
-dhtpin=17
+dhtpin=18
  
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -17,7 +17,7 @@ sensor = W1ThermSensor()
 sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
 while True:
     sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
-    if ( sensor2_value_t ) > 60:
+    if ( sensor2_value_t ) > 41:
        time.sleep(5)
        sensor2_value_h, sensor2_value_t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, dhtpin)
     print sensor2_value_h, sensor2_value_t
@@ -26,7 +26,7 @@ while True:
        sensor1_value = sensor.get_temperature()
        datetime=strftime("%d-%m-%Y %H:%M", time.localtime())
        print sensor1_value
-       if sensor1_value < 60 :
+       if sensor1_value < 41 :
           Sensors().InsertData(sensor1_value,sensor2_value_t, sensor2_value_h, datetime)
           print sensor1_value,sensor2_value_t, sensor2_value_h, datetime
        time.sleep(60)
